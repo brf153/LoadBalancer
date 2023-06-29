62119648ecfb4e149e12b07cd5ac588c2f4e5050
brf153 <153hsb@gmail.com>
2023-06-29 11:40:00 +0530

    starting main.go func

--- main.go
+++ main.go
@@ -4,14 +4,62 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
 
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request)
+}
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func (lb *LoadBalancer) getNextAvailableServer() Server{}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request){}
+
 func main() {
-	PORT:=":4000"
+	PORT := ":4000"
 	http.Handler("/", helloWorld)
-	err:= http.ListenAndServe(PORT,nil)
-	if err!=nil{
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Server started on port %v",PORT)
+	fmt.Printf("Server started on port %v", PORT)
 }
